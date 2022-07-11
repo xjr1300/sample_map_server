4cac3d01ca53aeb1ea9b67de9c1a2e1338f8edf2
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-07-11 19:11:18 +0900

    都道府県ハンドラがダミーでフィーチャー数を返却するように実装

--- map_server/src/main.rs
+++ map_server/src/main.rs
@@ -1,19 +1,39 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use database::connect_to_database;
+use dotenvy::dotenv;
+use sqlx::PgPool;
 
 async fn health_check() -> impl Responder {
     "Are you ready?"
 }
 
-async fn prefectures() -> HttpResponse {
-    HttpResponse::Ok().body("Prefectures")
+async fn prefectures(pool: web::Data<PgPool>) -> HttpResponse {
+    let result = sqlx::query!(
+        r#"
+        SELECT COUNT(*) count FROM prefectures
+        "#
+    )
+    .fetch_one(pool.as_ref())
+    .await;
+    match result {
+        Ok(result) => {
+            HttpResponse::Ok().body(format!("Prefecture count is {}", result.count.unwrap()))
+        }
+        Err(e) => HttpResponse::InternalServerError().body(format!("{}", e)),
+    }
 }
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    dotenv().ok();
+
+    let pool = web::Data::new(connect_to_database().await);
+
+    HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/prefectures", web::get().to(prefectures))
+            .app_data(pool.clone())
     })
     .bind(("127.0.0.1", 8080))?
     .run()
