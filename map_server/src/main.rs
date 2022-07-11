38881e14ab807f0257e70fa58ea0f5c3c58ba513
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-07-11 22:43:26 +0900

    都道府県APIを実装

--- map_server/src/main.rs
+++ map_server/src/main.rs
@@ -13,15 +13,20 @@ async fn health_check() -> impl Responder {
 async fn prefectures(pool: web::Data<PgPool>) -> HttpResponse {
     let result = sqlx::query!(
         r#"
-        SELECT COUNT(*) count FROM prefectures
-        "#
+        SELECT json_build_object(
+            'type', 'FeatureCollection',
+            'features', json_agg(ST_AsGeoJSON(p.*)::json)
+        ) as fc
+        FROM (
+            SELECT id, name, geom  FROM prefectures
+        ) p
+        "#,
     )
     .fetch_one(pool.as_ref())
     .await;
+
     match result {
-        Ok(result) => {
-            HttpResponse::Ok().body(format!("Prefecture count is {}", result.count.unwrap()))
-        }
+        Ok(result) => HttpResponse::Ok().json(result.fc.unwrap()),
         Err(e) => HttpResponse::InternalServerError().body(format!("{}", e)),
     }
 }
