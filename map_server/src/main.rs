c1689232116f02432f005a69c2d33014189cd5d4
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-07-11 18:56:01 +0900

    ダミーの都道府県ハンドラを実装

--- map_server/src/main.rs
+++ map_server/src/main.rs
@@ -1,13 +1,22 @@
-use actix_web::{web, App, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 async fn health_check() -> impl Responder {
     "Are you ready?"
 }
 
+async fn prefectures() -> HttpResponse {
+    HttpResponse::Ok().body("Prefectures")
+}
+
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/prefectures", web::get().to(prefectures))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
