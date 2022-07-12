e8ef9c8d7ad84eacbf0416e8e0a81830b1289a25
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-07-12 09:23:24 +0900

    register_post_officeのコマンドライン引数を定義

--- register_post_office/src/main.rs
+++ register_post_office/src/main.rs
@@ -1,3 +1,17 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// 国土数値情報の郵便局データを記録したShapeファイル。
+    #[clap(short, long, value_parser)]
+    file: String,
+
+    /// 郵便局データの空間参照ID。
+    #[clap(short, long, value_parser)]
+    srid: i32,
+}
+
 fn main() {
     println!("Hello, world!");
 }
