2b78e2b31737c5134f98a4fbe593a836d60c6709
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-07-31 16:09:49 +0900

    郵便局表にNOT NULL制約を追加

--- migrations/20220712050242_create_post_offices_table.up.sql
+++ migrations/20220712050242_create_post_offices_table.up.sql
@@ -1,12 +1,12 @@
 CREATE TABLE post_offices (
     id UUID PRIMARY KEY,
-    city_code CHAR(5),
-    category_code CHAR(2),
-    subcategory_code CHAR(5),
-    post_office_code CHAR(5),
-    name VARCHAR(40),
-    address VARCHAR(80),
-    geom geometry(POINT, 3857)
+    city_code CHAR(5) NOT NULL,
+    category_code CHAR(2) NOT NULL,
+    subcategory_code CHAR(5) NOT NULL,
+    post_office_code CHAR(5) NOT NULL,
+    name VARCHAR(40) NOT NULL,
+    address VARCHAR(80) NOT NULL,
+    geom geometry(POINT, 3857 NOT NULL)
 );
 
 CREATE INDEX idx_post_offices_city_code ON post_offices USING btree (city_code);
